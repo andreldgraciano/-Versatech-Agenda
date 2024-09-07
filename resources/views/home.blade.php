8cdc9278a1c790227abb71d5352ab1f5c1252b93
andreldgraciano <dedasgdias@gmail.com>
2024-09-07 12:15:50 -0300

    install tailwind

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.layout')
+@section('title', 'Home')
+
+@section('main')
+
+    <div>
+        <p>home - Minhas atividades, onde o admin e user podem ver suas atividades</p>
+    </div>
+
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Versatech - @yield('title')</title>
+    @vite('resources/css/app.css')
+    <link rel="icon" type="image/x-icon" href="/favicon.ico/">
+</head>
+<body>
+
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
